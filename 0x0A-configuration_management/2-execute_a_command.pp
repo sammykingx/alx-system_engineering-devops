e10e952a78301ffd3cf3b67acb23e0a5e11a264f
sammykingx <hellosammy@sammykingx.com.ng>
2022-12-26 18:27:02 -0800

    fixed the puppet-lint error

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 # A manifest to kill a running process killmenow from the process table
 # on every puppet run
 exec { 'killing a process using pkill':
-  command => 'pkill -9 killmenow',
+  command  => 'pkill -9 killmenow',
   path     => '/usr/bin:/bin',
   onlyif   => 'pgrep killmenow',
   provider => shell,
