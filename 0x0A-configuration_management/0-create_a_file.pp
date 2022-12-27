348e125137ea04386467dc65e1e28c0dd14a1a70
sammykingx <hellosammy@sammykingx.com.ng>
2022-12-26 18:23:11 -0800

    made a tweak to the files

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -4,6 +4,6 @@
 file { '/tmp/school':
   ensure  => present,
   mode    => '0744',
-  owner   => www-data,
-  group   => www-data,
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,9 @@
 # A manifest to kill a running process killmenow from the process table
 # on every puppet run
-exec { 'pkill -9 killmenow':
+exec { 'killing a process using pkill':
+  command => 'pkill -9 killmenow',
   path     => '/usr/bin:/bin',
-  onlyif   => pgrep killmenow,
+  onlyif   => 'pgrep killmenow',
   provider => shell,
 
 }
