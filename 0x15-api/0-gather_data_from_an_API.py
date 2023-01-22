64a9d1f4d017c3f440c29569ca05fd49a013aa6a
sammy <sammy@sammykingx.com.ng>
2023-01-22 02:35:54 +0000

    updated readme

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,8 +8,10 @@ if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com/'
     usr_id = r.get(url + 'users/{}'.format(sys.argv[1])).json()
     to_do = r.get(url + 'todos', params={'userId': sys.argv[1]}).json()
+#    print(to_do)
     completed = [title.get("title") for title in to_do if
                  title.get('completed') is True]
+    print(completed)
     print("Employee {} is done with tasks({}/{}):".format(usr_id.get("name"),
                                                           len(completed),
                                                           len(to_do)))
